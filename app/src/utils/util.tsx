7e9e28322ca3dcb55c804df66515d79103159191
Abdellah Allali - عبدالله علالي <aallali.1337@gmail.com>
2022-04-12 01:32:02 +0000

    [Progress] : doublefreethree detector ...

--- app/src/utils/util.tsx
+++ app/src/utils/util.tsx
@@ -1,5 +1,26 @@
 const { log } = console;
-
+let patterns = {
+  'doubleFreeThree': [
+    ['-1', '', '', '', '', '', '', ''],
+    ['', '1', '', '', '', '', '', ''],
+    ['', '', '1', '', '', '', '', ''],
+    ['', '', '', '0', '', '', '', ''],
+    ['', '', '', '0', '1', '1', '1', '-1'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+  ]
+}
+const paths: Record<string, string> = {
+  right: "diagTL:diagBL",
+  left: "diagTR:diagBR",
+  top: "diagBR:diagBL",
+  bottom: "diagTR:diagTL",
+  diagTR: "left:bottom",
+  diagTL: "right:bottom",
+  diagBL: "top:right",
+  diagBR: "top:left",
+};
 const directions: Record<string, number[]> = {
   top: [0, -1],
   right: [1, 0],
@@ -11,18 +32,18 @@ const directions: Record<string, number[]> = {
   left: [-1, 0],
 };
 const mirror: Record<string, string> = {
-    top: "bottom",
-    bottom: "top",
+  top: "bottom",
+  bottom: "top",
 
-    right: "left",
-    left: "right",
+  right: "left",
+  left: "right",
 
-    diagTR: "diagBL",
-    diagBL: "diagTR",
+  diagTR: "diagBL",
+  diagBL: "diagTR",
 
-    diagTL: "diagBR",
-    diagBR: "diagTL",
-  };
+  diagTL: "diagBR",
+  diagBR: "diagTL",
+};
 function countTotal(
   board: string[][],
   curY: number,
@@ -173,73 +194,104 @@ export function isForbiddenMove(
 
   return false;
 }
+const rotate: any = (matrix: string[][], turns: number | undefined) => {
 
+  // Make the rows to become cols (transpose)
+  const mtrx = matrix.map((_, i) => matrix.map(column => column[i]));
+  // Reverse each row to get a rotated matrix
+  const res = mtrx.map(row => row.reverse());
+  if (turns && turns > 0)
+    return rotate(res, --turns)
+  return res
+};
 export function isDoubleFreeThree(
   board: string[][],
   y: number,
   x: number,
   curr: string
 ) {
-  const copyBoard = JSON.parse(JSON.stringify(board)); // create deep copy for the board
-  const now: string = curr;
-  const enemy: string = now === "b" ? "w" : "b";
-  const dirs = Object.keys(directions);
-  const paths: Record<string, string> = {
-    right: "diagTL:diagBL",
-    left: "diagTR:diagBR",
-    top: "diagBR:diagBL",
-    bottom: "diagTR:diagTL",
-    diagTR: "left:bottom",
-    diagTL: "right:bottom",
-    diagBL: "top:right",
-    diagBR: "top:left",
-  };
-  for (let i = 0; i < dirs.length; i++) {
-    const dir = dirs[i];
-    let k = 2;
-    let tmpY = y;
-    let tmpX = x;
+  const doubleFreeThreePattern = patterns['doubleFreeThree']
+  let copyBoard = JSON.parse(JSON.stringify(board)) // create deep copy for the board
+  copyBoard[y][x] = curr
+  copyBoard = trim(copyBoard, '')
+  const enemy = curr === 'b' ? 'w' : curr
+  let allRotates: string[][][] = [doubleFreeThreePattern]
+  for (let i = 0; i < 3; i++) {
+    allRotates.push(rotate(doubleFreeThreePattern, i))
+  }
+  log("--------")
+  // allRotates.forEach((rot: string[][]) => {
+  //   log(rot)
+  //   log(findDoublePattern(copyBoard, curr, enemy, rot))
+  // })
+  log(allRotates[1])
+log(findDoublePattern(copyBoard, curr, enemy, allRotates[0]))
+log(findDoublePattern(copyBoard, curr, enemy, allRotates[1]))
+// log(findDoublePattern(copyBoard, curr, enemy, allRotates[2]))
 
-    while (k) {
-      tmpY += directions[dir][1];
-      tmpX += directions[dir][0];
-      if (copyBoard[tmpY][tmpX] !== now) break;
-      k--;
-    }
 
-    if (k === 0) {
-      log(
-        `Opposite paths for [${dir}] are : [${
-          paths[dir]
-        }] need to jump step ? [${(dir.includes("diag") && "yes") || "No"}]`
-      );
+  return true
 
-      for (let j = 0; j < 2; j++) {
-		tmpY = y;
-		tmpX = x;
-		if (dir.match(/diag/)) {
-			log("===>", mirror[dir])
-			
-			tmpY += directions[mirror[dir]][1];
-			tmpX += directions[mirror[dir]][0];
-	   
-		}
 
-        const path = paths[dir].split(":")[j];
 
-        k = 3;
-        while (k) {
-          tmpY += directions[path][1];
-          tmpX += directions[path][0];
-		 
-          log(path, copyBoard[tmpY][tmpX]);
-		  copyBoard[tmpY][tmpX] = 'r'
-          k--;
+}
+
+
+export function findDoublePattern(cboard: string[][], curr: string, enemy: string, doubleFreeThreePattern: string[][]) {
+
+  let k = 0
+
+  const board = JSON.parse(JSON.stringify(cboard))
+  // doubleFreeThreePattern = trim(doubleFreeThreePattern, '')
+
+  log(board, doubleFreeThreePattern)
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      k = 0
+      for (let y = 0; y < doubleFreeThreePattern.length; y++) {
+        for (let x = 0; x < doubleFreeThreePattern[y].length; x++) {
+          // log(`i:${i},j:${j},y:${y},x:${x},`)
+          if ((x + j) < board[0].length && (y + i) < board.length) {
+            if ((doubleFreeThreePattern[y][x] === '1' && board[y + i][x + j] === curr)
+              || (doubleFreeThreePattern[y][x] === '0' && board[y + i][x + j] === '')
+              || (doubleFreeThreePattern[y][x] === '-1' && board[y + i][x + j] !== enemy))
+              k++
+          }
         }
       }
-
-      return copyBoard
+      if (k === 9)
+        return true
+     
     }
   }
-  return copyBoard
+
+
+  return false
 }
+function crop(board: string[][], ymin: number, ymax: number, xmin: number, xmax: number) {
+
+
+  return board.slice(ymin - 1, ymax + 1).map(function (row) {
+    return row.slice(xmin - 1, xmax + 2)
+  });
+}
+
+function trim(board: string[][], toTrim: string) {
+  let cmin, rmin, cmax, rmax
+
+  cmin = board[0].length
+  rmin = board.length
+  cmax = -1
+  rmax = -1
+
+  for (let r = 0; r < board.length; r++)
+    for (let c = 0; c < board[0].length; c++)
+      if (board[r][c] !== toTrim) {
+        if (cmin > c) cmin = c;
+        if (cmax < c) cmax = c;
+        if (rmin > r) rmin = r;
+        if (rmax < r) rmax = r;
+      }
+  return crop(board, rmin, rmax, cmin, cmax);
+}
+
