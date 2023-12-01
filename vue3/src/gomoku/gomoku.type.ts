4009dfa3df910fe4a94f14190072f15ff04b256c
3dida <44850191+aallali@users.noreply.github.com>
2023-12-01 23:00:34 +0100

    [+] : TRepport + PartialBy type : gomoku.type.ts

--- vue3/src/gomoku/gomoku.type.ts
+++ vue3/src/gomoku/gomoku.type.ts
@@ -1,3 +1,4 @@
+import type { TDirection } from "./common/directions"
 
 export type TMtx = (0 | 1 | 2)[][]
 export type Nb = number
@@ -5,3 +6,18 @@ export type P = 1 | 2
 export type TColor = "b" | "w"
 export type TPoint = { x: Nb, y: Nb }
 export type TMode = "1337" | "normal"
+
+export interface TRepport {
+    directions: {
+        [key in TDirection]: {
+            consecutives: number,
+            bounds: number
+        };
+    }
+    isWin?: boolean
+    score: number
+    isOpenFour?: boolean
+}
+
+// export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
