9064de6c2570d7bc3472817b6ec68c4e9488058e
3dida <44850191+aallali@users.noreply.github.com>
2023-12-03 01:50:42 +0100

    [chore] : vars names optimize duplic BestMove_NormalMode : @go/Ai

--- vue3/src/gomoku/ai.ts
+++ vue3/src/gomoku/ai.ts
@@ -9,10 +9,9 @@ import type { Nb, PartialBy, TColor, TMtx, TPoint, TRepport } from "./gomoku.typ
  */
 function EvalPiece(matrix: TMtx, x: Nb, y: Nb, player: TColor) {
     const turn = player == "b" ? 1 : 2
-
     const size = matrix.length;
-
     const repport = { score: 0 } as TRepport
+
     for (let i = 0; i < 4; i++) {
         const dir = directions[i];
         repport.directions = {} as TRepport["directions"]
@@ -20,13 +19,16 @@ function EvalPiece(matrix: TMtx, x: Nb, y: Nb, player: TColor) {
             consecutives: 0,
             bounds: 0,
         };
+
         let coord = MoveDirection(dir, x, y);
         while (1) {
             if (!validXY(size, coord.x, coord.y)) {
                 repport.directions[dir].bounds++;
                 break
             }
+
             const cell = matrix[coord.x][coord.y];
+
             if (cell != turn && cell != 0) {
                 repport.directions[dir].bounds++;
                 break;
@@ -44,28 +46,29 @@ function EvalPiece(matrix: TMtx, x: Nb, y: Nb, player: TColor) {
                 repport.directions[dir].bounds++;
                 break
             }
+
             const cell = matrix[coord.x][coord.y];
             if (cell != turn && cell != 0) {
                 repport.directions[dir].bounds++;
                 break;
-            } else if (cell == 0) {
+            } else if (cell == 0)
                 break;
-            }
 
             repport.directions[dir].consecutives++;
             coord = MoveDirection(DirectionMirror[dir] as TDirection, coord.x, coord.y)
         }
+        const consecutives = repport.directions[dir].consecutives;
+        const bounds = repport.directions[dir].bounds;
 
-        repport.score += GetScore(repport.directions[dir].consecutives, repport.directions[dir].bounds);
-        if (repport.directions[dir].consecutives >= 4) {
+        if (consecutives >= 4) {
             repport.isWin = true;
+            repport.score = 111110 /* 10 000*/
             break
         }
-
-        if (repport.directions[dir].consecutives >= 3 && repport.directions[dir].bounds == 0) {
+        if (consecutives >= 3 && bounds == 0)
             repport.isOpenFour = true;
-        }
 
+        repport.score += GetScore(consecutives, bounds);
     }
 
     delete (repport as PartialBy<TRepport, "directions">).directions
@@ -109,26 +112,33 @@ function GetScore(count: Nb, bound: Nb) {
  */
 export function BestMove_NormalMode(matrix: TMtx, player: TColor, validMoves: TPoint[]) {
     const opponent = player == "b" ? "w" : "b"
-    const myBestMove = { score: 0 } as TPoint & { score: number }
-    const enemyBestMove = { score: 0 } as TPoint & { score: number }
+    const myBestMove = {} as TPoint
+    const enemyBestMove = {} as TPoint
+
+    myBestMove.score = -1;
+    enemyBestMove.score = -1
 
     for (let i = 0; i < validMoves.length; i++) {
-        const { x, y } = validMoves[i]
+        const { x, y } = validMoves[i];
 
-        const myScore = EvalPiece(matrix, x, y, player).score
-        const hisScore = EvalPiece(matrix, x, y, opponent).score
+        const offensiveMove = EvalPiece(matrix, x, y, player);
+        const deffensiveMove = EvalPiece(matrix, x, y, opponent);
 
-        if (myScore > myBestMove.score) {
+        if (offensiveMove.isWin)
+            return { x, y, score: offensiveMove.score }
+
+        if (offensiveMove.score > myBestMove.score) {
             myBestMove.x = x
             myBestMove.y = y
-            myBestMove.score = myScore
+            myBestMove.score = offensiveMove.score
         }
-        if (hisScore > enemyBestMove.score) {
+
+        if (deffensiveMove.score > enemyBestMove.score) {
             enemyBestMove.x = x
             enemyBestMove.y = y
-            enemyBestMove.score = hisScore
+            enemyBestMove.score = deffensiveMove.score
         }
     }
 
-    return myBestMove.score >= enemyBestMove.score || myBestMove.score >= 5000 ? myBestMove : enemyBestMove
+    return myBestMove.score >= enemyBestMove.score ? myBestMove : enemyBestMove
 }
