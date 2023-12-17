76de3ce3b7224caedd95dad183f14a2383c98715
3dida <44850191+aallali@users.noreply.github.com>
2023-12-17 22:26:01 +0100

    [feat] : :construction: `whatIsTheBestMove` for 1337 mode

---
+++ vue3/src/gomoku/modes/1337/bestmove.ts
@@ -0,0 +1,83 @@
+import type { P, TMtx } from "@/gomoku/types/gomoku.type";
+import { findValidSpots } from "./moveValidity";
+import { MoveRepport } from "./prior_moves";
+
+export function whatIsTheBestMove(matrix: TMtx, turn: P, player1Captrues: number, player2Captures: number) {
+
+    const repporter = new MoveRepport()
+    repporter.setMatrix(matrix)
+    repporter.setTurn(turn)
+    let availableSpots = []
+    const validSpots = findValidSpots(matrix, turn, "1337")
+
+    for (let i = 0; i < validSpots.length; i++) {
+        const move = validSpots[i]
+        repporter.setPoint({ x: move.x, y: move.y });
+        if (repporter.isNearBy()) {
+            const repport = repporter.repportObj()
+            if (!repport.willBCaptured)
+                availableSpots.push({ x: move.x, y: move.y, ...repport })
+        }
+    }
+
+    function changePosition<T>(array: T[], valueToMove: T, newPosition: number): T[] {
+        const currentValueIndex = array.indexOf(valueToMove);
+
+        if (currentValueIndex === -1) {
+            console.log(`Value ${valueToMove} not found in the array.`);
+            return array; // Value not found, return the original array
+        }
+
+        // Remove the value from its current position
+        const removedValue = array.splice(currentValueIndex, 1)[0];
+
+        // Insert the value at the new position
+        array.splice(newPosition, 0, removedValue);
+
+        return array;
+    }
+
+    // Define the order of priority for fields
+    let fieldPriority: string[] = [
+        'isWinBy5',
+        'blockWinBy5',
+        'open4', 'blockOpen4',
+        'isCapture', 'captureSetup',
+        'blockCapture',
+        'open3', 'blockOpen3',
+        'isAlginedWithPeer',
+    ];
+
+    if (player1Captrues === 4)
+        fieldPriority = changePosition(fieldPriority, 'isCapture', 0);
+    else if (player2Captures === 4)
+        fieldPriority = changePosition(fieldPriority, 'blockCapture', 0);
+
+
+    // Custom comparator function
+    const compareFunction = (a: { [x: string]: any; }, b: { [x: string]: any; }): number => {
+        for (const field of fieldPriority) {
+            if (field === 'score') {
+                return b[field] - a[field]
+            }
+            if (field === 'isAlginedWithPeer') {
+                if (a[field][0] === b[field][0])
+                    return a[field][1] - b[field][1]
+                return b[field][0] - a[field][0]
+            }
+            const aValue = a[field] ? 1 : 0;
+            const bValue = b[field] ? 1 : 0;
+
+            if (aValue !== bValue) {
+                return bValue - aValue; // Sort in descending order
+            }
+        }
+
+        return 0; // Objects are equal based on the specified fields
+    };
+
+    const sortedArray = [...availableSpots].sort(compareFunction);
+
+
+    return sortedArray[0]
+}
