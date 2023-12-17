65b76775eed4edad2018477619d40691b33b2827
3dida <44850191+aallali@users.noreply.github.com>
2023-12-17 22:27:58 +0100

    [feat] : add analysis from store : @C/AiAnalayse.vue

--- vue3/src/components/AiAnalayse.vue
+++ vue3/src/components/AiAnalayse.vue
@@ -1,6 +1,13 @@
 <template>
     <fieldset>
         <h4 style="margin: 0px">Ai Analyse:</h4>
-        <textarea cols="35" rows="35" style="font-size: 15px"></textarea>
+        <textarea cols="35" rows="35" style="font-size: 15px" v-bind:value="analyse"></textarea>
     </fieldset>
 </template>
+
+<script lang="ts" setup>
+
+import { useGame } from '@/store';
+const analyse = useGame((state) => state.analyse)
+
+</script>
