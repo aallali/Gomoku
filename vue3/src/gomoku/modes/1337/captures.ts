05ddff0e68e7f05209d5faafd8a0d3d1afe9d348
3dida <44850191+aallali@users.noreply.github.com>
2023-12-22 02:07:41 +0100

    [doc] :  isLineBreakableByAnyCapture : @1337/captures.ts

--- vue3/src/gomoku/modes/1337/captures.ts
+++ vue3/src/gomoku/modes/1337/captures.ts
@@ -39,21 +39,25 @@ export function extractCaptures(matrix: TMtx, moves: TPoint[], player: P): TPoin
 }
 
 /**
-*  @description takes 2 params:<br>
-*  1: list of capture pieces of Player-A <br>
-*  2: list of win stones that formed the win line for Player-B
-*   
-*  verify if any of the win stones exists withing the capture pieces <br>
-*  the purpose of this check, to prevent declare a win of a player untill <br>
-*  check that his win row is not breakable by any capture from the enemy
-*/
+ * Verifies if any of the win stones exists within the capture pieces.
+ *
+ * @param captures - List of capture pieces of Player-A.
+ * @param stonesLine - List of win stones that formed the win line for Player-B.
+ * @returns A boolean indicating whether the win line is breakable by any capture.
+ *
+ * The purpose of this check is to prevent declaring a win for a player until
+ * ensuring that their win row is not breakable by any capture from the enemy.
+ */
 export function isLineBreakableByAnyCapture(captures: TPoint[][], stonesLine: TPoint[]): boolean {
-    return !!captures
-        .filter(m => m
-            .find(c => stonesLine
-                .map(l => l.x.toString() + '.' + l.y)
-                .join(",")
-                .includes(c.x + '.' + c.y))).length
+    const hasBreakableCapture = !!captures.find((captureList) =>
+        captureList.some((capture) =>
+            stonesLine.some((stone) => `${stone.x}.${stone.y}` === `${capture.x}.${capture.y}`)
+        )
+    );
+
+    return hasBreakableCapture;
+}
+
 
 /**
  * Applies captures if any at the specified position in the matrix.
