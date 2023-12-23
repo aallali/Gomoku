213764bc7a351a3ec4ec01bfb76b70668b8e7c14
3dida <44850191+aallali@users.noreply.github.com>
2023-12-23 18:37:12 +0100

    [chore] : increase nearby range by 1 : @MoveRepport.isNearBy

--- vue3/src/gomoku/modes/1337/prior_moves.ts
+++ vue3/src/gomoku/modes/1337/prior_moves.ts
@@ -299,7 +299,7 @@ export class MoveRepport {
     isNearBy(_matrix?: TMtx, cell?: TPoint) {
         const [matrix, _x, _y] = [_matrix || this.matrix, cell?.x || this.x, cell?.y || this.y]
         return forEachDirection((dir) => {
-            const rawPath = ScrapLine(matrix, 0, 1, _x, _y, dir);
+            const rawPath = ScrapLine(matrix, 0, 2, _x, _y, dir);
             if (/1|2/.test(rawPath.substring(1))) {
                 return true
             }
