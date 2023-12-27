a45f5313929e6871ae2b5c1d925b583b020dbf8e
3dida <44850191+aallali@users.noreply.github.com>
2023-12-27 17:53:15 +0100

    [update] : add time depth limit (alpha_beta_tt) : @MiniMax

--- vue3/src/gomoku/modes/1337/MiniMax.ts
+++ vue3/src/gomoku/modes/1337/MiniMax.ts
@@ -1,48 +1,56 @@
 import type GO from "@/gomoku/GO";
-import type { P } from "@/gomoku/types/gomoku.type";
 
 export class Minimax {
-  static minimax(node: typeof GO, depth: number, alpha: number, beta: number, maximizingPlayer: boolean): number {
-    if (depth === 0 || node.winner) {
-      if (node.winner === 1) return -99999
-      if (node.winner === 2) return 99999
+  static timeoutMillis = 550; // time depth limit
+  static startMillis = Date.now();
 
-      return  0
-    }
-    
-    node.generateChildren();
-  
-    if (maximizingPlayer) {
-      let maxEval = Number.NEGATIVE_INFINITY;
-  
-      for (const child of node.children) {
-        const score = Minimax.minimax(child, depth - 1, alpha, beta, false);
-        maxEval = Math.max(maxEval, score);
-        alpha = Math.max(alpha, score);
-  
-        if (beta <= alpha) {
-          break; // Beta cutoff
-        }
+  static minimax(node: typeof GO, depth: number, alpha: number, beta: number, maximizingPlayer: boolean): number {
+
+
+    const recursiveMinimax = (node: typeof GO, depth: number, alpha: number, beta: number, maximizingPlayer: boolean): number => {
+      if (depth === 0 || node.winner || Date.now() - this.startMillis > Minimax.timeoutMillis) {
+        if (node.winner === 1) return 33333 - (10 - depth);
+        if (node.winner === 2) return 88888 - (10 - depth);
+
+        return node.lastPlayed.score || 0
       }
-  
-      return maxEval;
-    } else {
-      let minEval = Number.POSITIVE_INFINITY;
-  
-      for (const child of node.children) {
-        const score = Minimax.minimax(child, depth - 1, alpha, beta, true);
-        minEval = Math.min(minEval, score);
-        beta = Math.min(beta, score);
-  
-        if (beta <= alpha) {
-          break; // Alpha cutoff
+
+      node.generateChildren();
+
+
+      if (maximizingPlayer) {
+        let maxEval = Number.NEGATIVE_INFINITY;
+
+        for (const child of node.children) {
+          const score = recursiveMinimax(child, depth - 1, alpha, beta, false);
+          maxEval = Math.max(maxEval, score);
+          alpha = Math.max(alpha, score);
+
+          if (beta <= alpha) {
+            break; // Beta cutoff
+          }
         }
+
+        return maxEval - (5 - depth);
+      } else {
+        let minEval = Number.POSITIVE_INFINITY;
+
+        for (const child of node.children) {
+          const score = recursiveMinimax(child, depth - 1, alpha, beta, true);
+          minEval = Math.min(minEval, score);
+          beta = Math.min(beta, score);
+
+          if (beta <= alpha) {
+            break; // Alpha cutoff
+          }
+        }
+
+        return minEval + (5 - depth); // Apply penalty for minimizing player
       }
-  
-      return minEval;
-    }
+    };
+
+    return recursiveMinimax(node, depth, alpha, beta, maximizingPlayer);
   }
-  
 
   static findBestMove(initialState: typeof GO, depth: number, _delete?: boolean): any {
     const root = initialState
@@ -51,26 +59,28 @@ export class Minimax {
     let maximizingPlayer = true
     let bestValue = maximizingPlayer ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
     root.generateChildren();
-    console.log("----------->", root.turn)
     console.time("AllmovesMinimax:")
-
-    for (const child of root.children) {
-      // console.time("moveMinimax:")
-      const score = Minimax.minimax(child, depth - 1, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY, !maximizingPlayer) + (child.lastPlayed.score || 0);
-
-      // console.log(child.lastPlayed, score - (child.lastPlayed.score || 0))
-      if (
-        (maximizingPlayer && score > bestValue)
-        || (!maximizingPlayer && score < bestValue)) {
-        bestValue = score;
-        bestMove = child;
+    console.log(`Total moves checked : ${root.children.length}`)
+    if (root.children.length > 1 && root.moves.length >= 5) {
+      this.startMillis = Date.now();
+      for (const child of root.children) {
+        const score = Minimax.minimax(child, depth - 1, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY, !maximizingPlayer) + (child.lastPlayed.score || 0);
+        if (
+          (maximizingPlayer && score > bestValue)
+          || (!maximizingPlayer && score < bestValue)) {
+          bestValue = score;
+          bestMove = child;
+        }
       }
-      // console.timeEnd("moveMinimax:")
+    } else {
+      bestValue = root.children[0].lastPlayed.score || -1;
+      bestMove = root.children[0];
     }
-    console.log(`Best for (${root.turn===1? 'Black' : 'White'})::`,bestMove?.lastPlayed, bestValue)
+    
+    console.log(`Best by Heuristic: {x: ${root.children[0].lastPlayed.x}, y: ${root.children[0].lastPlayed.y}}`)
+    console.log(`Best by MiniMax: {x: ${bestMove?.lastPlayed.x}, y: ${bestMove?.lastPlayed.y}}`, bestValue)
     console.timeEnd("AllmovesMinimax:")
- 
-    return bestMove;
+    return bestMove?.lastPlayed;
   }
 }
 
