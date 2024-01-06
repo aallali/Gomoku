4046b4a59f8342eea3e19c13d0b323125e439623
3dida <44850191+aallali@users.noreply.github.com>
2024-01-06 20:33:17 +0100

    [style]

--- gomoku-v2/src/components/AiAnalayse.vue
+++ gomoku-v2/src/components/AiAnalayse.vue
@@ -1,6 +1,6 @@
 <template>
     <fieldset>
-        <h4 style="margin: 0px">Ai Analyse:</h4>
+        <h4 style="margin: 0px">Move Analyse:</h4>
         <br/>
         <div style="display: flex;flex-direction: column;">
         <textarea cols="35" rows="22" style="font-size: 15px" v-bind:value="analyse"></textarea>
